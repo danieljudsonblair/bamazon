0a0c696aa45d91cd33bd9c2c04a3a458415e93a5
s1191 <danieljudsonblair@gmail.com>
2019-01-29 20:38:54 -0500

    Challenge 1 completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,10 @@
+require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var divider = "--------------------------------------------"
-// create the connection information for the sql database
-var connection = mysql.createConnection({
+var connection = mysql.createConnection({  
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: process.env.PSWD,
   database: "bamazon_db"
 });
@@ -22,11 +15,82 @@ connection.connect(function(err) {
   });
 
   function displayProducts() {
-    connection.query("SELECT * FROM products",function(err,res) {
+    connection.query("SELECT item_id, product_name, price FROM products",function(err,res) {
         if (err) throw err;
-        console.log(divider);
-        res.map(i => console.log("Item ID: " + i.item_id + ", Item: " + i.product_name + ", Price: $" + i.price + "\n" + divider));
-        connection.end(); 
-
+        console.table(res);
+        placeOrder();
     })
 }
+
+function placeOrder() {
+  inquirer
+  .prompt([
+    {
+      name: "id",
+      message: "Enter the ID number of the product you would like to buy: ",
+      type: "input",
+      validate: function(value) {
+        return new Promise(function(resolve, reject) {
+          connection.query("SELECT COUNT(item_id) AS id FROM products WHERE item_id = ?", value, function(err,res) {
+            if (err) throw err;
+            if (res[0].id === 1) {
+              resolve(true);
+            } else {
+              resolve("Please enter a valid Item ID.");
+            }
+          });
+        }).then(function(value) {
+          return value;
+        });
+      }
+    },
+    {
+      name: "qty",
+      message: "Enter the number of units you would like to buy: ",
+      type: "input",
+      validate: function(val) {
+        if (!isNaN(val)) {
+          return true;
+        } else {
+          return "Please enter a number.";
+        }
+      }
+    }
+  ])
+  .then(function(ans) { 
+    connection.query("SELECT * FROM products WHERE item_id = ?", ans.id, function(err, res) {
+      if (err) throw err;
+      if (ans.qty <= res[0].stock_quantity) {
+        var newQty = res[0].stock_quantity - ans.qty;
+        connection.query("UPDATE products SET stock_quantity = ?  WHERE item_id = ?", [newQty, ans.id], function() {
+          if (err) throw err;
+          var totalPrice = res[0].price * ans.qty;
+          console.log("Your Total is $" + totalPrice + " for " + ans.qty + " " + res[0].product_name);
+          nextOrder();
+        });
+      } else {
+        console.log("Insufficient quantity! (We're sold out)");
+        nextOrder();
+      }
+    });
+  });
+}
+
+function nextOrder () {
+  inquirer
+  .prompt({
+    name: "yn",
+    message: "Would you like to place another order?",
+    type: "list",
+    choices: ["YES", "N0"]
+  })
+  .then(function(ans) {
+    if (ans.yn === "YES") {
+      displayProducts();
+    } else {
+      connection.end();
+    }
+  });
+}
+
+
--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -7,7 +7,7 @@ CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(50) NOT NULL,
-  price DOUBLE PRECISION default 0.00,
+  price DECIMAL (10,2) default 0.00,
   stock_quantity INT default 0,
   PRIMARY KEY (id)
 );
